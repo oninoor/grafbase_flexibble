539a572f7ca6c9826b8e286c034274e8b38fa733
Oni Noor <rezpector335@gmail.com>
2023-06-25 19:26:39 +0700

    setup grafbase and nextauth

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,7 +1,50 @@
-import React from "react";
+"use client";
+
+import { getProviders, signIn } from "next-auth/react";
+import { useState, useEffect } from "react";
+import { Button } from "./ui/button";
+
+type Provider = {
+  id: string;
+  name: string;
+  type: string;
+  signinUrl: string;
+  callbackUrl: string;
+  signinUrlParams?: Record<string, string> | undefined;
+};
+
+type Providers = Record<string, Provider>;
 
 const AuthProviders = () => {
-  return <div>AuthProviders</div>;
+  const [providers, setProviders] = useState<Providers | null>(null);
+
+  useEffect(() => {
+    const fetchProviders = async () => {
+      const res = await getProviders();
+
+      console.log(res);
+
+      setProviders(res);
+    };
+
+    fetchProviders();
+  }, []);
+
+  if (providers) {
+    return (
+      <div>
+        {Object.values(providers).map((provider: Provider, i) => (
+          <Button
+            className="capitalize"
+            key={i}
+            onClick={() => signIn(provider?.id)}
+          >
+            {provider.id}
+          </Button>
+        ))}
+      </div>
+    );
+  }
 };
 
 export default AuthProviders;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,9 +3,10 @@ import Image from "next/image";
 import { NavLinks } from "@/constants";
 import { link } from "fs";
 import AuthProviders from "./AuthProviders";
+import { getCurrentUser } from "@/lib/session";
 
-const Navbar = () => {
-  const session = {};
+const Navbar = async () => {
+  const session = await getCurrentUser();
 
   return (
     <nav>
@@ -25,9 +26,18 @@ const Navbar = () => {
         </div>
 
         <div className="flexCenter gap-4">
-          {session ? (
+          {session?.user ? (
             <>
-              UserPhoto
+              {session.user.image && (
+                <Image
+                  src={session.user.image}
+                  width={40}
+                  height={40}
+                  className="rounded-full"
+                  alt={session.user.name}
+                />
+              )}
+
               <Link href="/create-project">Share Work</Link>
             </>
           ) : (
